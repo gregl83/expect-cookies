c4c6c903eec0d46021193b9066410d2385d881bf
Gregory Langlais <general@gregorylanglais.com>
2015-11-14 15:34:54 -0500

    package skeleton

---
+++ src/ExpectCookie.js
@@ -0,0 +1,5 @@
+function ExpectCookie() {
+  // todo
+}
+
+module.exports = ExpectCookie;
---
+++ test/ExpectCookie.js
@@ -0,0 +1,9 @@
+var should = require('should');
+
+var ExpectCookie = require('../');
+
+describe('ExpectCookie', function() {
+  it('todo', function(done) {
+    done();
+  });
+});
