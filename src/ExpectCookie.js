28a453b7aa9d925e060d8155d8b46396668a3d22
Gregory Langlais <general@gregorylanglais.com>
2015-11-14 16:58:21 -0500

    usage through main assertion construct
    - main construct returns assertion function
    - easy methods allow for less configuration

--- src/ExpectCookie.js
+++ src/ExpectCookie.js
@@ -1,5 +1,83 @@
-function ExpectCookie() {
+/**
+ * Construct for cookie assertion
+ *
+ * @param {object} options
+ * @param {string} options.name
+ * @param {string} options.value
+ * @param {string} options.domain
+ * @param {string} options.path
+ * @param {string} options.expires
+ * @param {string} options.max-age
+ * @param {string} options.secure
+ * @param {string} options.httponly
+ * @returns {function} assertion
+ * @constructor
+ */
+function ExpectCookie(options) {
   // todo
+
+
+  function assertion(res) {
+
+  }
+
+  return assertion;
 }
+
+
+/**
+ * Assert cookie is set
+ *
+ * @param {object} options
+ * @param {boolean} initial time cookie is set
+ * @returns {function}
+ */
+ExpectCookie.set = function(options, initial) {
+  if ('boolean' !== typeof initial) initial = true;
+
+  // todo
+
+  return ExpectCookie(options);
+};
+
+
+/**
+ * Assert cookie is NOT set
+ *
+ * @param options {object}
+ * @returns {function}
+ */
+ExpectCookie.notSet = function(options) {
+  // todo
+
+  return ExpectCookie(options);
+};
+
+
+/**
+ * Assert cookie has been reset
+ *
+ * @param options {object}
+ * @returns {function}
+ */
+ExpectCookie.reset = function(options) {
+  // todo
+
+  return ExpectCookie(options);
+};
+
+
+/**
+ * Assert cookie expires or max-age has increased
+ *
+ * @param options {object}
+ * @returns {function}
+ */
+ExpectCookie.renew = function(options) {
+  // todo
+
+  return ExpectCookie(options);
+};
+
 
 module.exports = ExpectCookie;
--- test/ExpectCookie.js
+++ test/ExpectCookie.js
@@ -1,9 +1,25 @@
 var should = require('should');
 
-var ExpectCookie = require('../');
+var Cookie = require('../');
 
-describe('ExpectCookie', function() {
-  it('todo', function(done) {
+describe('Cookie', function() {
+  it('has been set', function(done) {
+    done();
+  });
+
+  it('has been set for the first time', function(done) {
+    done();
+  });
+
+  it('has been reset', function(done) {
+    done();
+  });
+
+  it('has been renewed', function(done) {
+    done();
+  });
+
+  it('is NOT set', function(done) {
     done();
   });
 });
