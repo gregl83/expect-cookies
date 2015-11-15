a583e13e83e5fc7899d0e5664ccd155c2698cde1
Gregory Langlais <general@gregorylanglais.com>
2015-11-15 03:49:46 -0500

    basic tests and assertions stack/array arg

--- src/ExpectCookie.js
+++ src/ExpectCookie.js
@@ -1,24 +1,27 @@
 /**
- * Construct for cookie assertion
+ * Construct cookie assertion (function)
  *
- * @param {object} options
+ * @param {object} options cookie options
  * @param {string} options.name
  * @param {string} options.value
- * @param {string} options.domain
- * @param {string} options.path
- * @param {string} options.expires
- * @param {string} options.max-age
- * @param {string} options.secure
- * @param {string} options.httponly
+ * @param {string} [options.domain]
+ * @param {string} [options.path]
+ * @param {string} [options.expires] UTC string using date.toUTCString()
+ * @param {number} [options.max-age]
+ * @param {boolean} [options.secure]
+ * @param {boolean} [options.httponly]
+ * @param {string} [options.signed] secret to check against value
+ * @param {function(req, res)[]} assertions ran within returned assertion function
  * @returns {function} assertion
  * @constructor
  */
-function ExpectCookie(options) {
-  // todo
+function ExpectCookie(options, assertions) {
+  if (!Array.isArray(assertions)) assertions = [];
 
+  // todo add assertions for provided options
 
   function assertion(res) {
-
+    // todo loop over assertions and return if error
   }
 
   return assertion;
@@ -26,57 +29,68 @@ function ExpectCookie(options) {
 
 
 /**
- * Assert cookie is set
+ * Assert cookie is set for first time
  *
- * @param {object} options
- * @param {boolean} initial time cookie is set
+ * @param {object} options for cookie
+ * @param {function(req, res)[]} assertions
  * @returns {function}
  */
-ExpectCookie.set = function(options, initial) {
-  if ('boolean' !== typeof initial) initial = true;
+ExpectCookie.first = function(options, assertions) {
+  if (!Array.isArray(assertions)) assertions = [];
 
-  // todo
+  // todo add first assertion
 
-  return ExpectCookie(options);
+  return ExpectCookie(options, assertions);
 };
 
 
 /**
  * Assert cookie is NOT set
  *
- * @param options {object}
+ * @param {object} options for cookie
+ * @param {function(req, res)[]} assertions
  * @returns {function}
  */
-ExpectCookie.notSet = function(options) {
-  // todo
+ExpectCookie.notSet = function(options, assertions) {
+  if (!Array.isArray(assertions)) assertions = [];
 
-  return ExpectCookie(options);
+  // todo add notSet assertion
+
+  return ExpectCookie(options, assertions);
 };
 
 
 /**
  * Assert cookie has been reset
  *
- * @param options {object}
+ * @param {object} options for cookie
+ * @param {object} compare cookie
+ * @param {function(req, res)[]} assertions
  * @returns {function}
  */
-ExpectCookie.reset = function(options) {
-  // todo
+ExpectCookie.reset = function(options, compare, assertions) {
+  if (!Array.isArray(assertions)) assertions = [];
 
-  return ExpectCookie(options);
+  // todo add reset assertion
+
+  return ExpectCookie(options, assertions);
 };
 
 
 /**
  * Assert cookie expires or max-age has increased
  *
- * @param options {object}
+ * @param {object} options for cookie
+ * @param {object} compare cookie
+ * @param {function(req, res)[]} assertions
  * @returns {function}
  */
-ExpectCookie.renew = function(options) {
-  // todo
+ExpectCookie.renew = function(options, compare, assertions) {
+  if (!Array.isArray(assertions)) assertions = [];
 
-  return ExpectCookie(options);
+  // todo add renew assertion
+
+  return ExpectCookie(options, assertions);
 };
 
 
--- test/ExpectCookie.js
+++ test/ExpectCookie.js
@@ -1,25 +1,428 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var request = require('supertest');
 var should = require('should');
 
 var Cookie = require('../');
 
+
 describe('Cookie', function() {
-  it('has been set', function(done) {
-    done();
+  describe('.first', function() {
+    it('asserts true if cookie is set and was NOT already set', function(done) {
+      var expires = new Date();
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.cookie('name', 'value', {domain: 'domian.com', path: '/', expires: expires, "max-age": 60, secure: 1, httpOnly: true, signed: true});
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .set("Cookie", "n=v")
+        .expect(function(res) {
+          var assertion = Cookie.first({
+            "name": 'name',
+            "value": 'value',
+            "domain": 'domain.com',
+            "path": '/',
+            "expires": expires.toUTCString(),
+            "max-age": 60,
+            "secure": true,
+            "httponly": true,
+            "signed": secret
+          });
+
+          should(assertion).not.throw();
+
+          return assertion(res);
+        })
+        .end(done);
+    });
+
+    it('asserts false if cookie is set but was already set', function(done) {
+      var expires = new Date();
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.cookie('name', 'value', {domain: 'domian.com', path: '/', expires: expires, "max-age": 60, secure: 1, httpOnly: true, signed: true});
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .set("Cookie", "name=value")
+        .expect(function(res) {
+          var assertion = Cookie.first({
+            "name": 'name',
+            "value": 'value',
+            "domain": 'domain.com',
+            "path": '/',
+            "expires": expires.toUTCString(),
+            "max-age": 60,
+            "secure": true,
+            "httponly": true,
+            "signed": secret
+          });
+
+          should(assertion).throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
+
+    it('asserts false if cookie is NOT set', function(done) {
+      var expires = new Date();
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .set("Cookie", "n=v")
+        .expect(function(res) {
+          var assertion = Cookie.first({
+            "name": 'name',
+            "value": 'value',
+            "domain": 'domain.com',
+            "path": '/',
+            "expires": expires.toUTCString(),
+            "max-age": 60,
+            "secure": true,
+            "httponly": true,
+            "signed": secret
+          });
+
+          should(assertion).throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
   });
 
-  it('has been set for the first time', function(done) {
-    done();
+  describe('.reset', function() {
+    it('asserts true if cookie is set and was already set', function(done) {
+      var expires = new Date();
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.cookie('name', 'reset', {domain: 'domian.com', path: '/', expires: expires, "max-age": 60, secure: 1, httpOnly: true, signed: true});
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .set("Cookie", "name=value")
+        .expect(function(res) {
+          var assertion = Cookie.reset({
+            "name": 'name',
+            "value": 'reset',
+            "domain": 'domain.com',
+            "path": '/',
+            "expires": expires.toUTCString(),
+            "max-age": 60,
+            "secure": true,
+            "httponly": true,
+            "signed": secret
+          });
+
+          should(assertion).not.throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
+
+    it('asserts false if cookie is NOT set', function(done) {
+      var expires = new Date();
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .set("Cookie", "n=v")
+        .expect(function(res) {
+          var assertion = Cookie.reset({
+            "name": 'name',
+            "value": 'reset',
+            "domain": 'domain.com',
+            "path": '/',
+            "expires": expires.toUTCString(),
+            "max-age": 60,
+            "secure": true,
+            "httponly": true,
+            "signed": secret
+          });
+
+          should(assertion).throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
   });
 
-  it('has been reset', function(done) {
-    done();
+  describe('.renew', function() {
+    it('asserts true if cookie expiration is greater than already set cookie', function(done) {
+      var compareExpires = new Date();
+      var expires = new Date(compareExpires + 60000);
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.cookie('name', 'value', {domain: 'domian.com', path: '/', expires: expires, secure: 1, httpOnly: true, signed: true});
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .set("Cookie", "name=value")
+        .expect(function(res) {
+          var assertion = Cookie.renew({
+            "name": 'name',
+            "value": 'value',
+            "domain": 'domain.com',
+            "path": '/',
+            "expires": expires.toUTCString(),
+            "secure": true,
+            "httponly": true,
+            "signed": secret
+          },{
+            "expires": expires.toUTCString()
+          });
+
+          should(assertion).not.throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
+
+    it('asserts false if cookie expiration is less than or equal to already set cookie', function(done) {
+      var compareExpires = new Date();
+      var expires = new Date(compareExpires - 60000);
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.cookie('name', 'value', {domain: 'domian.com', path: '/', expires: expires, secure: 1, httpOnly: true, signed: true});
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .set("Cookie", "name=value")
+        .expect(function(res) {
+          var assertion = Cookie.renew({
+            "name": 'name',
+            "value": 'value',
+            "domain": 'domain.com',
+            "path": '/',
+            "expires": expires.toUTCString(),
+            "secure": true,
+            "httponly": true,
+            "signed": secret
+          },{
+            "expires": compareExpires.toUTCString()
+          });
+
+          should(assertion).throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
+
+    it('asserts true if cookie max-age is greater than already set cookie', function(done) {
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.cookie('name', 'value', {domain: 'domian.com', path: '/', "max-age": 120, secure: 1, httpOnly: true, signed: true});
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .set("Cookie", "name=value")
+        .expect(function(res) {
+          var assertion = Cookie.renew({
+            "name": 'name',
+            "value": 'value',
+            "domain": 'domain.com',
+            "path": '/',
+            "max-age": 120,
+            "secure": true,
+            "httponly": true,
+            "signed": secret
+          },{
+            "max-age": 60
+          });
+
+          should(assertion).not.throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
+
+    it('asserts false if cookie max-age is less than or equal to already set cookie', function(done) {
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.cookie('name', 'value', {domain: 'domian.com', path: '/', "max-age": 60, secure: 1, httpOnly: true, signed: true});
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .set("Cookie", "name=value")
+        .expect(function(res) {
+          var assertion = Cookie.renew({
+            "name": 'name',
+            "value": 'value',
+            "domain": 'domain.com',
+            "path": '/',
+            "max-age": 60,
+            "secure": true,
+            "httponly": true,
+            "signed": secret
+          },{
+            "max-age": 120
+          });
+
+          should(assertion).throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
+
+    it('asserts false if cookie is NOT set', function(done) {
+      var compareExpires = new Date();
+      var expires = new Date(compareExpires - 60000);
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .set("Cookie", "n=v")
+        .expect(function(res) {
+          var assertion = Cookie.reset({
+            "name": 'name',
+            "value": 'reset',
+            "domain": 'domain.com',
+            "path": '/',
+            "expires": expires.toUTCString(),
+            "max-age": 120,
+            "secure": true,
+            "httponly": true,
+            "signed": secret
+          }, {
+            "expires": compareExpires.toUTCString(),
+            "max-age": 60
+          });
+
+          should(assertion).throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
   });
 
-  it('has been renewed', function(done) {
-    done();
-  });
+  describe('.notSet', function() {
+    it('asserts true if cookie is NOT set', function(done) {
+      var secret = 'secret';
 
-  it('is NOT set', function(done) {
-    done();
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .expect(function(res) {
+          var assertion = Cookie.notSet({"name": 'name'});
+
+          should(assertion).not.throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
+
+    it('asserts false if cookie IS set', function(done) {
+      var secret = 'secret';
+
+      var app = express();
+
+      app.use(cookieParser(secret, {signed: true}));
+
+      app.get('/', function(req, res) {
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .expect(function(res) {
+          var assertion = Cookie.notSet({"name": 'name'});
+
+          should(assertion).throw();
+
+          assertion(res);
+        })
+        .end(done);
+    });
   });
 });
